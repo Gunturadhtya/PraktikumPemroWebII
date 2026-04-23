41c373fa9b7a39f512a62d42cf1e94552bdbd854
Guntur <gunturpdng@gmail.com>
2026-04-23 15:58:35 +0800

    feat: complete soal 2 modul 2

--- modul_2/PRAK202.php
+++ modul_2/PRAK202.php
@@ -1,12 +1,56 @@
 <?php
 
-?>
+$fields = [
+    "nama"  => "Nama", 
+    "nim" => "NIM", 
+    "gender" => "Jenis Kelamin"
+];
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    foreach ($fields as $key => $label) {
+        $input = trim($_POST[$key]);
+        
+        if (empty($input)) {
+            $errors[$key] = "$label tidak boleh kosong";
+        } else {
+            $values[$key] = $input;
+        }
+    }
+}
+
+?>
 
 <html>
     <head>
         <title>Soal 2 Modul 2</title>
+        <style>
+            span{
+                color: #DC143C;
+            }
+        </style>
     </head>
     <body>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            Nama: <input type="text" name="nama">
+            <span class="error">* <?php echo $errors["nama"];?></span><br>
+
+            NIM: <input type="text" name="nim">
+            <span class="error">* <?php echo $errors["nim"];?></span><br>
+
+            Jenis Kelamin: <span class="error">* <?php echo $errors["gender"];?></span><br>
+            <input type="radio" name="gender" value="Laki-Laki">
+            <label for="html">Laki-Laki</label><br>
+            <input type="radio" name="gender" value="Perempuan">
+            <label for="css">Perempuan</label><br>
+
+            <input type="submit">
+        </form>
+        <p>
+            <?php
+                echo $values["nama"] . "<br>"; 
+                echo $values["nim"] . "<br>"; 
+                echo $values["gender"] . "<br>"; 
+            ?>
+        </p>
     </body>
 </html>

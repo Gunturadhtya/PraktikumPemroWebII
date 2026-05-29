2cc41da415efe29ce13bec127be6eafd4449a1e0
guntur <gunturpdng@gmail.com>
2026-05-29 09:34:06 +0800

    feat(modul 5) : implement index.php

---
+++ modul_5/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Library System</title></head>
+<body>
+    <h1>Sistem Informasi Perpustakaan</h1>
+    <ul>
+        <li><a href="Buku/Buku.php">Kelola Buku</a></li>
+        <li><a href="Member/Member.php">Kelola Member</a></li>
+        <li><a href="Peminjaman/Peminjaman.php">Kelola Peminjaman</a></li>
+    </ul>
+</body>
+</html>

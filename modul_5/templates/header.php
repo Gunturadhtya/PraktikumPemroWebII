82b5fb341daa221a35aa9535e3f60b5f6b76aff9
guntur <gunturpdng@gmail.com>
2026-05-30 19:04:42 +0800

    fix(modul 5): wrong title page

--- modul_5/templates/header.php
+++ modul_5/templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perpustakaan Mudzakir</title>
+    <title>Perpustakaan Guntur</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .bg-dashboard { background-color: #d0d4dc; }
